0039781d040d23b1c50a14c8a6f28bbd4be5c772
Vitalii Kyba <k190583@gmail.com>
2024-02-09 00:03:51 +0200

    part of main page layaut implemented

---
+++ src/components/MainHeroSection/MainHeroSection.tsx
@@ -0,0 +1,12 @@
+import styles from "./MainHeroSection.module.scss"
+const MainHeroSection = () => {
+    return (
+        <div className={styles.mainHeroSection}>
+            <h1>Українське завжди
+                <span> краще</span>
+            </h1>
+        </div>
+    );
+};
+
+export default MainHeroSection;
---
+++ src/components/MainImagesSection/MainImagesSection.tsx
@@ -0,0 +1,12 @@
+import styles from "./MainImagesSection.module.scss"
+const MainImagesSection = () => {
+    return (
+        <div className={styles.mainImagesSection}>
+            {/*<h1>Українське завжди*/}
+            {/*    <span>краще</span>*/}
+            {/*</h1>*/}
+        </div>
+    );
+};
+
+export default MainImagesSection;
