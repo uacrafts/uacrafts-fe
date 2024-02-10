98c2d84b0ac8625b4306be0ecd06c2e65d25fe2a
Demirov Vlad <demirov.vlad@gmail.com>
2024-02-10 17:26:50 +0200

    Conflicts resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.module.scss";
 import HeaderLayout from "./layouts/HeaderLayout/HeaderLayout.tsx";
 import CallbackModal from "./components/modals/CallbackModal/CallbackModal.tsx";
+import Main from "./components/Main/Main.tsx";
 
 function App() {
   return (
-    <HeaderLayout>
-      <CallbackModal />
-    </HeaderLayout>
+    <>
+      <HeaderLayout>
+        <Main />
+      </HeaderLayout>
+    </>
   );
 }
 
