1a969e15319306186550dec4aeb4129893f77126
Demirov Vlad <demirov.vlad@gmail.com>
2024-02-15 11:16:01 +0200

    callback functionality integration completed

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,17 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-// const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
-// const graphqlServerUrl = "https://uacraft-be.vercel.app/graphql/";
-//
-// const httpLink = createHttpLink({
-//   uri: corsAnywhereUrl + graphqlServerUrl,
-//   fetchOptions: {
-//     mode: "cors", // Make sure 'cors' mode is set
-//   },
-// });
-
 const client = new ApolloClient({
-  uri: "https://uacraft-be-git-uac-5-uac-14-dmytros-projects-b07917bd.vercel.app/graphql/",
+  uri: "https://backend-uacraft.koyeb.app/graphql/",
   cache: new InMemoryCache(),
 });
 
