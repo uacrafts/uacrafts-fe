8cea454ccb5a4ebd74dfd6d6d0cf083d903fc442
Vitalii Kyba <k190583@gmail.com>
2024-03-26 19:31:21 +0200

    commit

---
+++ src/components/Hero/HeroSectionHero.tsx
@@ -0,0 +1,10 @@
+import styles from "./HeroSectionHero.module.scss"
+const HeroSectionHero = () => {
+    return (
+        <div className={styles.heroSectionHero}>
+            <h1>CraftLand</h1>
+        </div>
+    );
+};
+
+export default HeroSectionHero;
