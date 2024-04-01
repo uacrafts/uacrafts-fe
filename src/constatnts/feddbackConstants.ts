69de7fa14e2a7c417531eaa3812630ed05e660f1
Demirov Vlad <demirov.vlad@gmail.com>
2024-04-01 22:57:46 +0300

    feedback avatar image fixes

--- src/constatnts/feddbackConstants.ts
+++ src/constatnts/feddbackConstants.ts
@@ -1,29 +1,33 @@
 import { Feedback } from "../types";
+import avatar1 from "../assets/images/avatar1.png";
+import avatar2 from "../assets/images/avatar2.png";
+import avatar3 from "../assets/images/avatar3.png";
+import avatar4 from "../assets/images/avatar4.png";
 
 export const feedbacks: Feedback[] = [
   {
-    avatar: "avatar1.png",
+    avatar: avatar1,
     name: "Aнна Бобровська",
     nickname: "@annabobrivska",
     comment:
       "Вражаючий асортимент унікальних виробів, які вражають своєю красою та якістю виконання. Задоволена своєю покупкою і обов'язково повернусь за новими враженнями!\"",
   },
   {
-    avatar: "avatar2.png",
+    avatar: avatar2,
     name: "Андрій Федоров",
     nickname: "@anhdshjhsd",
     comment:
       'Цей магазин - справжній скарб для поціновувачів унікальних виробів. Їхні  роботи вражають своєю красою та майстерністю. Рекомендую кожному, хто цінує витвори мистецтва та бажає підтримати ручну працю."',
   },
   {
-    avatar: "avatar3.png",
+    avatar: avatar3,
     name: "Дмитро Юнг",
     nickname: "@admutro",
     comment:
       'Захоплюючий магазин, де кожен виріб вражає своєю оригінальністю та майстерністю виконання. Я в захваті від унікальних предметів, що пропонуються тут! Раджу всім, хто цінує ручну працю та неповторність."',
   },
   {
-    avatar: "avatar4.png",
+    avatar: avatar4,
     name: "Тетяна Погребна",
     nickname: "@pogrebna",
     comment:
