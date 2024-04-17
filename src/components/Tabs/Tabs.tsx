be9ae61f7324d4e93cfbd8a50d503f510e36eec1
Demirov Vlad <demirov.vlad@gmail.com>
2024-04-18 00:08:11 +0800

    react-scroll fixes

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Tabs.module.scss";
 import { Link } from "react-scroll";
 
@@ -14,6 +14,21 @@ interface TabsProps {
 
 const Tabs: React.FC<TabsProps> = ({ tabs }) => {
   const [activeTab, setActiveTab] = useState<number>(tabs[0].id);
+  const [offset, setOffset] = useState<number>(0);
+
+  useEffect(() => {
+    const updateOffset = () => {
+      const screenWidth = window.innerWidth;
+
+      // calculate offset based on screen dimensions
+      const newOffset = screenWidth < 768 ? -170 : -116;
+
+      setOffset(newOffset);
+    };
+
+    // Initial update
+    updateOffset();
+  }, []);
 
   return (
     <div className={styles.tabsContainer}>
@@ -21,7 +36,7 @@ const Tabs: React.FC<TabsProps> = ({ tabs }) => {
         {tabs.map((tab, index) => (
           <Link
             to={tab.sectionId}
-            offset={-170}
+            offset={offset}
             key={tab.id}
             className={`${styles.tab} ${activeTab === tab.id ? styles.active : ""}`}
             onClick={() => setActiveTab(tabs[index].id)}
